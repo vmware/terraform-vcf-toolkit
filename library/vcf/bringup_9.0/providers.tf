ccc5a2f40aa80637faa606ae4f6fa1454ab268aa
Steven Tumolo <steven.tumolo@broadcom.com>
2025-09-11 12:23:49 -0400

    vcf9.0/bringup - first draft of bring up, no ops.fleetmanater or vcfa

---
+++ library/vcf/bringup_9.0/providers.tf
@@ -0,0 +1,21 @@
+# --------------------------------------------------------------- #
+# Providers for Nested ESXi Lab v1.0
+# --------------------------------------------------------------- #
+terraform {
+  required_providers {
+    vsphere = {
+      source  = "vmware/vcf"
+      version = "0.17.1"
+    }
+  }
+}
+
+# --------------------------------------------------------------- #
+# vCenter Settings
+# --------------------------------------------------------------- #
+provider "vcf" {
+installer_host = "vcf-installer.jpmc.lab"
+installer_password = "VMware1!VMware1!"
+installer_username = "admin"
+allow_unverified_tls = true # Required for self-signed
+}
